99fca8270c7c4ece64ca0508936e2ad9169aff50
Spencer Childress <samussiah@gmail.com>
2017-08-15 16:32:01 -0400

    closer still

--- build/paneledOutlierExplorer.js
+++ build/paneledOutlierExplorer.js
@@ -452,6 +452,62 @@ function allEqual(args) {
     return true;
 }
 
+function brushMarks(chart, points, lines) {
+    var extent$$1 = chart.config.extent,
+        x0 = extent$$1[0][0],
+        // top left x-coordinate
+    y0 = extent$$1[1][1],
+        // top left y-coordinate
+    x1 = extent$$1[1][0],
+        // bottom right x-coordinate
+    y1 = extent$$1[0][1],
+        // bottom right y-coordinate
+    top = { x0: x1, y0: y0, x1: x0, y1: y0 },
+        right = { x0: x1, y0: y1, x1: x1, y1: y0 },
+        bottom = { x0: x0, y0: y1, x1: x1, y1: y1 },
+        left = { x0: x0, y0: y0, x1: x0, y1: y1 },
+        sides = [top, right, bottom, left];
+
+    //brush points
+    var brushedPoints = points.filter(function (d) {
+        return x0 <= d.values.x && y0 >= d.values.y && x1 >= d.values.x && y1 <= d.values.y;
+    }).data().map(function (d) {
+        return d.key1;
+    }),
+        allPoints = d3.select(chart.config.element).selectAll('.point-supergroup g.point circle').classed('brushed selected', false);
+    allPoints.filter(function (d) {
+        return brushedPoints.indexOf(d.key1) > -1;
+    }).classed('brushed', true).each(function () {
+        d3.select(this.parentNode).moveToFront();
+    });
+
+    //brushed lines
+    var brushedLines = lines.filter(function (d, i) {
+        var intersection = false;
+        d.lines.forEach(function (line, j) {
+            sides.forEach(function (side, k) {
+                if (!intersection) {
+                    intersection = doLineSegmentsIntersect({ x: line.x0, y: line.y0 }, { x: line.x1, y: line.y1 }, { x: side.x0, y: side.y0 }, { x: side.x1, y: side.y1 });
+                }
+            });
+        });
+        return intersection;
+    }).data().map(function (d) {
+        return d.id;
+    }),
+        allLines = d3.select(chart.config.element).selectAll('.line-supergroup g.line path').classed('brushed', false);
+    allLines.filter(function (d) {
+        return brushedLines.indexOf(d.id) > -1;
+    }).classed('brushed', true).each(function () {
+        d3.select(this.parentNode).moveToFront();
+    });
+    allPoints.filter(function (d) {
+        return brushedLines.indexOf(d.id) > -1;
+    }).classed('selected', true).each(function () {
+        d3.select(this.parentNode).moveToFront();
+    });
+}
+
 function brush() {
     var chart = this;
 
@@ -487,72 +543,20 @@ function brush() {
         d3.selectAll(chart.config.element).selectAll('.wc-chart').each(function (d) {
             if (d.measure !== measure) d.overlay.call(d.brush.clear());
         });
-
-        //brush
         chart.config.extent = chart.package.brush.extent();
-        var extent$$1 = chart.config.extent,
-            x0 = extent$$1[0][0],
-            // top left x-coordinate
-        y0 = extent$$1[1][1],
-            // top left y-coordinate
-        x1 = extent$$1[1][0],
-            // bottom right x-coordinate
-        y1 = extent$$1[0][1],
-            // bottom right y-coordinate
-        top = { x0: x1, y0: y0, x1: x0, y1: y0 },
-            right = { x0: x1, y0: y1, x1: x1, y1: y0 },
-            bottom = { x0: x0, y0: y1, x1: x1, y1: y1 },
-            left = { x0: x0, y0: y0, x1: x0, y1: y1 },
-            sides = [top, right, bottom, left];
 
-        //brushed points
-        var brushedPoints = points.filter(function (d) {
-            return x0 <= d.values.x && y0 >= d.values.y && x1 >= d.values.x && y1 <= d.values.y;
-        }).data().map(function (d) {
-            return d.key1;
-        }),
-            allPoints = d3.select(chart.config.element).selectAll('.point-supergroup g.point circle').classed('brushed selected', false);
-        allPoints.filter(function (d) {
-            return brushedPoints.indexOf(d.key1) > -1;
-        }).classed('brushed', true).each(function () {
-            d3.select(this.parentNode).moveToFront();
-        });
-
-        //brushed lines
-        var brushedLines = lines.filter(function (d, i) {
-            var intersection = false;
-            d.lines.forEach(function (line, j) {
-                sides.forEach(function (side, k) {
-                    if (!intersection) {
-                        intersection = doLineSegmentsIntersect({ x: line.x0, y: line.y0 }, { x: line.x1, y: line.y1 }, { x: side.x0, y: side.y0 }, { x: side.x1, y: side.y1 });
-                    }
-                });
-            });
-            return intersection;
-        }).data().map(function (d) {
-            return d.id;
-        }),
-            allLines = d3.select(chart.config.element).selectAll('.line-supergroup g.line path').classed('brushed', false);
-        allLines.filter(function (d) {
-            return brushedLines.indexOf(d.id) > -1;
-        }).classed('brushed', true).each(function () {
-            d3.select(this.parentNode).moveToFront();
-        });
-        allPoints.filter(function (d) {
-            return brushedLines.indexOf(d.id) > -1;
-        }).classed('selected', true).each(function () {
-            d3.select(this.parentNode).moveToFront();
-        });
+        //brush marks
+        brushMarks(chart, points, lines);
     }).on('brushend', function () {});
 
     //Initialize brush on brush overlay.
     this.package.overlay.call(this.package.brush);
 
     if (!this.config.extent) this.config.extent = this.package.brush.extent();
-    if (this.config.extent !== this.package.brush.extent()) {
+    if (this.config.extent[0][0] !== this.package.brush.extent()[0][0] || this.config.extent[0][1] !== this.package.brush.extent()[0][1] || this.config.extent[1][0] !== this.package.brush.extent()[1][0] || this.config.extent[1][1] !== this.package.brush.extent()[1][1]) {
         this.package.brush.extent(this.config.extent);
-        console.log(this.config.extent);
-        console.log(this.package.brush.extent());
+        this.package.overlay.call(this.package.brush);
+        brushMarks(chart, points, lines);
     }
 }
 

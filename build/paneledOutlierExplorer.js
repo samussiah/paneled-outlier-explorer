6fc217e08a85f31cf8bf47991f9a2fc90ce39cf1
Jeremy Wildfire <jwildfire@gmail.com>
2017-10-09 09:28:53 -0700

    add css prefix. fix #55

--- build/paneledOutlierExplorer.js
+++ build/paneledOutlierExplorer.js
@@ -13,7 +13,7 @@
     Controls
     \--------------------------------------------------------------------------------------***/
 
-                '#controls-header {' +
+                '#safety-outlier-explorer #controls-header {' +
                     '    margin: 0;' +
                     '    overflow: hidden;' +
                     '    background-color: #333;' +
@@ -25,41 +25,52 @@
                     '    padding: 14px 16px;' +
                     '    box-sizing: border-box;' +
                     '}',
-                '#left-side {' + '    width: 24%;' + '    float: left;' + '}',
-                '#left-side > * {' + '    width: 100%;' + '    display: inline-block;' + '}',
-                '#left-side .wc-controls {' + '    padding: 10px 0;' + '}',
-                '#left-side .wc-controls .control-group {' +
+                '#safety-outlier-explorer #left-side {' +
+                    '    width: 24%;' +
+                    '    float: left;' +
+                    '}',
+                '#safety-outlier-explorer #left-side > * {' +
+                    '    width: 100%;' +
+                    '    display: inline-block;' +
+                    '}',
+                '#safety-outlier-explorer #left-side .wc-controls {' + '    padding: 10px 0;' + '}',
+                '#safety-outlier-explorer #left-side .wc-controls .control-group {' +
                     '    float: left;' +
                     '    clear: left;' +
                     '    margin: 0 0 2px 0;' +
                     '}',
-                '#left-side .wc-controls .control-group > * {' +
+                '#safety-outlier-explorer #left-side .wc-controls .control-group > * {' +
                     '    display: inline-block;' +
                     '    margin-left: 3px;' +
                     '}',
-                '#left-side #measure-list-container {' + '   padding:0' + '}',
-                '#left-side #measure-list-container #measure-list-header {' +
+                '#safety-outlier-explorer #left-side #measure-list-container {' +
+                    '   padding:0' +
+                    '}',
+                '#safety-outlier-explorer #left-side #measure-list-container #measure-list-header {' +
                     '    font-size: 150%;' +
                     '    border-top: 1px solid lightgray;' +
                     '    font-weight: lighter;' +
                     '    padding: 14px 0;' +
                     '}',
-                '#left-side #measure-list-container #measure-list-checkbox {' +
+                '#safety-outlier-explorer #left-side #measure-list-container #measure-list-checkbox {' +
                     '    margin: 5px;' +
                     '}',
-                '#left-side #measure-list-container #measure-list {' +
+                '#safety-outlier-explorer #left-side #measure-list-container #measure-list {' +
                     '    list-style-type: none;' +
                     '    font-weight: lighter;' +
                     '}',
-                '#left-side #measure-list-container .measure-item {' + '}',
-                '#left-side #measure-list-container .measure-item-container {' + '}',
-                '#left-side #measure-list-container .measure-checkbox {' + '    margin: 5px;' + '}',
+                '#safety-outlier-explorer #left-side #measure-list-container .measure-item {' + '}',
+                '#safety-outlier-explorer #left-side #measure-list-container .measure-item-container {' +
+                    '}',
+                '#safety-outlier-explorer #left-side #measure-list-container .measure-checkbox {' +
+                    '    margin: 5px;' +
+                    '}',
 
                 /***--------------------------------------------------------------------------------------\
     Navigation
     \--------------------------------------------------------------------------------------***/
 
-                'ul#navigation-bar {' +
+                '#safety-outlier-explorer ul#navigation-bar {' +
                     '    list-style-type: none;' +
                     '    margin: 0;' +
                     '    padding: 0;' +
@@ -68,7 +79,7 @@
                     '    width: 75%;' +
                     '    float: right;' +
                     '}',
-                'ul#navigation-bar li.navigation {' +
+                '#safety-outlier-explorer ul#navigation-bar li.navigation {' +
                     '    float: left;' +
                     '    cursor: pointer;' +
                     '    font-size: 150%;' +
@@ -78,9 +89,13 @@
                     '    padding: 14px 16px;' +
                     '    text-decoration: none;' +
                     '}',
-                'ul#navigation-bar li.navigation.active {' + '    background-color: #111;' + '}',
-                'ul#navigation-bar li.navigation:hover {' + '    background-color: #111;' + '}',
-                'ul#navigation-bar li.navigation#Listing-nav.brushed {' +
+                '#safety-outlier-explorer ul#navigation-bar li.navigation.active {' +
+                    '    background-color: #111;' +
+                    '}',
+                '#safety-outlier-explorer ul#navigation-bar li.navigation:hover {' +
+                    '    background-color: #111;' +
+                    '}',
+                '#safety-outlier-explorer ul#navigation-bar li.navigation#Listing-nav.brushed {' +
                     '    color: orange;' +
                     '}',
 
@@ -88,22 +103,22 @@
     Charts
     \--------------------------------------------------------------------------------------***/
 
-                'div.wc-layout.wc-small-multiples#Charts {' +
+                '#safety-outlier-explorer div.wc-layout.wc-small-multiples#Charts {' +
                     '    width: 75%;' +
                     '    float: right;' +
                     '    padding-top: 10px;' +
                     '}',
-                'div.wc-layout.wc-small-multiples#Charts > div.wc-chart {' +
+                '#safety-outlier-explorer div.wc-layout.wc-small-multiples#Charts > div.wc-chart {' +
                     '    padding-right: 1em;' +
                     '}',
-                'div.wc-layout.wc-small-multiples#Charts > div.wc-chart.expanded {' +
+                '#safety-outlier-explorer div.wc-layout.wc-small-multiples#Charts > div.wc-chart.expanded {' +
                     '    width: 100%;' +
                     ' }',
-                'div.wc-layout.wc-small-multiples#Charts > div.wc-chart .wc-chart-title {' +
+                '#safety-outlier-explorer div.wc-layout.wc-small-multiples#Charts > div.wc-chart .wc-chart-title {' +
                     '    text-align: left;' +
                     '    padding-left: 10px;' +
                     '}',
-                'div.wc-layout.wc-small-multiples#Charts > div.wc-chart .chart-button {' +
+                '#safety-outlier-explorer div.wc-layout.wc-small-multiples#Charts > div.wc-chart .chart-button {' +
                     '    float: right;' +
                     '    cursor: pointer;' +
                     '    border: 1px solid black;' +
@@ -113,11 +128,11 @@
                     '    margin-left: 5px;' +
                     '    visibility:hidden;' +
                     '}',
-                'div.wc-layout.wc-small-multiples#Charts > div.wc-chart .chart-button:hover {' +
+                '#safety-outlier-explorer div.wc-layout.wc-small-multiples#Charts > div.wc-chart .chart-button:hover {' +
                     '    background: black;' +
                     '    color: white;' +
                     '}',
-                'div.wc-layout.wc-small-multiples#Charts > div.wc-chart .x.axis text.axis-title{' +
+                '#safety-outlier-explorer div.wc-layout.wc-small-multiples#Charts > div.wc-chart .x.axis text.axis-title{' +
                     'display:none;' +
                     '}',
 
@@ -125,32 +140,40 @@
     Listing
     \--------------------------------------------------------------------------------------***/
 
-                'div.wc-chart#Listing {' +
+                '#safety-outlier-explorer div.wc-chart#Listing {' +
                     '    width: 75%;' +
                     '    float: right;' +
                     '    padding-top: 10px;' +
                     '}',
-                'div.wc-chart#Listing table {' + '    padding-left: 10px;' + '}',
+                '#safety-outlier-explorer div.wc-chart#Listing table {' +
+                    '    padding-left: 10px;' +
+                    '}',
 
                 /***--------------------------------------------------------------------------------------\
     General styles
     \--------------------------------------------------------------------------------------***/
 
-                '.hidden {' + '    display: none !important;' + '}',
-                'circle.brushed {' +
+                '#safety-outlier-explorer .hidden {' + '    display: none !important;' + '}',
+                '#safety-outlier-explorer circle.brushed {' +
                     '    stroke: orange;' +
                     '    stroke-width: 2px;' +
                     '    fill: black;' +
                     '    r: 4px;' +
                     '}',
-                'path.brushed {' +
+                '#safety-outlier-explorer path.brushed {' +
                     '    stroke: orange;' +
                     '    stroke-width: 3px;' +
                     '    stroke-opacity: 1;' +
                     '}',
-                'path.hover {' + '    stroke: orange;' + '    stroke-opacity: 1;' + '}',
-                'circle.selected {' + '    stroke: orange;' + '    fill: black;' + '}',
-                'tr.brushed {' + '    background: orange !important;' + '}'
+                '#safety-outlier-explorer path.hover {' +
+                    '    stroke: orange;' +
+                    '    stroke-opacity: 1;' +
+                    '}',
+                '#safety-outlier-explorer circle.selected {' +
+                    '    stroke: orange;' +
+                    '    fill: black;' +
+                    '}',
+                '#safety-outlier-explorer tr.brushed {' + '    background: orange !important;' + '}'
             ],
             style = document.createElement('style');
         style.type = 'text/css';
